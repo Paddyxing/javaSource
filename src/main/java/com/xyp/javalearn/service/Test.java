a094ac987c6f36b919a870a16f7fd4fd6693e8db
Paddyxing <yapengxing@sohu-inc.com>
2022-03-22 18:58:10 +0800

    qs

--- src/main/java/com/xyp/javalearn/service/Test.java
+++ src/main/java/com/xyp/javalearn/service/Test.java
@@ -7,6 +7,7 @@ public class Test {
 
     public static void main(String args[]){
         LocalDateTime now = LocalDateTime.now();
+        System.out.println(true ? 1 : false ? 0 : 2);
 
         now.plusYears(2);
         now.minusMonths(1);
